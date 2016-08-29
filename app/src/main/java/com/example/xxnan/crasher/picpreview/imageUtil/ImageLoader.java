126dc408afd11757e708feb87b44f053be6e133e
xxnan <android_framework@163.com>
2016-08-29 23:13:56 +0800

    second

---
+++ app/src/main/java/com/example/xxnan/crasher/picpreview/imageUtil/ImageLoader.java
@@ -0,0 +1,150 @@
+package com.example.xxnan.crasher.picpreview.imageUtil;
+
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ * Created by Administrator on 2016/8/29.
+ */
+public class ImageLoader {
+    private static ImageLoader imageLoader;
+    private static final int DEAFULT_MSG = 0X110;
+    private static final int DEAFULT_THREAD_COUNT = 10;
+    private LruCache<String, Bitmap> lruCache;
+    private ExecutorService mThreadPool;
+    private LinkedList<Runnable> taskQuene;
+    private Handler mUiHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            ImageBean bean = (ImageBean) msg.obj;
+            ImageView imageView = bean.imageView;
+            String path = bean.path;
+            Bitmap bitmap = bean.bitmap;
+            if (path.equals(imageView.getTag().toString()))
+                imageView.setImageBitmap(bitmap);
+        }
+    };
+    private Thread mThreadPoolThread;
+    private Handler poolHandler;
+
+    private ImageLoader() {
+        mThreadPoolThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                poolHandler = new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        super.handleMessage(msg);
+                        if (msg.what == 0X110) {
+                            mThreadPool.execute(getTask());
+                        }
+                    }
+                };
+                Looper.loop();
+            }
+        });
+        mThreadPoolThread.start();
+        int maxCache = (int) (Runtime.getRuntime().maxMemory() / 8);
+        lruCache = new LruCache<String, Bitmap>(maxCache) {
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getRowBytes() * value.getHeight();
+            }
+        };
+        mThreadPool = Executors.newFixedThreadPool(DEAFULT_THREAD_COUNT);
+        taskQuene = new LinkedList<Runnable>();
+    }
+
+    private Runnable getTask() {
+        return taskQuene.removeLast();
+    }
+
+    /**
+     * 缓存图片
+     *
+     * @param key
+     * @param bitmap
+     */
+    public void putCache(String key, Bitmap bitmap) {
+        lruCache.put(key, bitmap);
+    }
+
+    /**
+     * 取出缓存图片
+     *
+     * @param key
+     * @return
+     */
+    public Bitmap getCache(String key) {
+        return lruCache.get(key);
+    }
+
+    public static ImageLoader getInstance() {
+        if (imageLoader == null) {
+            synchronized (ImageLoader.class) {
+                if (imageLoader == null)
+                    imageLoader = new ImageLoader();
+            }
+        }
+        return imageLoader;
+    }
+
+    /**
+     * 加载图片
+     *
+     * @param path
+     * @param imageView
+     */
+    public void loadImage(String path, final ImageView imageView) {
+        imageView.setTag(path);
+        Bitmap bitmap = lruCache.get(path);
+        if (bitmap != null) {
+            Message msg = mUiHandler.obtainMessage();
+            ImageBean bean = new ImageBean();
+            bean.bitmap = bitmap;
+            bean.path = path;
+            bean.imageView = imageView;
+            msg.obj = bean;
+            mUiHandler.sendMessage(msg);
+        } else {
+            //开启线程加载图片
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    ImageSize imageSize = getImageSize(imageView);
+                }
+            };
+            taskQuene.add(runnable);
+            Message msg = poolHandler.obtainMessage();
+            msg.what = DEAFULT_MSG;
+            poolHandler.sendMessage(msg);
+        }
+    }
+
+    private ImageSize getImageSize(ImageView imageView) {
+
+        return null;
+    }
+
+    class ImageBean {
+        String path;
+        Bitmap bitmap;
+        ImageView imageView;
+    }
+
+    class ImageSize {
+        int width;
+        int height;
+    }
+}
