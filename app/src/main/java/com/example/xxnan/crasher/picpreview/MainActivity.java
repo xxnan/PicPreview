83a5c185a9ee90470cc134ce475c3d5b774fd819
xxnan <android_framework@163.com>
2016-08-30 10:18:09 +0800

    mainactivity

--- app/src/main/java/com/example/xxnan/crasher/picpreview/MainActivity.java
+++ app/src/main/java/com/example/xxnan/crasher/picpreview/MainActivity.java
@@ -1,21 +1,44 @@
 package com.example.xxnan.crasher.picpreview;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RelativeLayout relativeLayout;
+    private TextView dirName,dirCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        intData();
+    }
 
+    /**
+     * 从ContentResolver获取图片
+     */
+    private void intData() {
+        Uri uri= MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        ContentResolver contentResolver=getContentResolver();
+//        contentResolver.query(uri,MediaStore.Images.Media.DATA,MediaStore.Images.Media._ID
+//                ,MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,MediaStore.Images.Thumbnails.DATA,MediaStore.Images.Thumbnails.IMAGE_ID
+//                ,MediaStore.Images.Media.DATE_MODIFIED + " desc ")
+    }
+
+    private void initView() {
+        recyclerView= (RecyclerView) findViewById(R.id.pic_recycleview);
+        relativeLayout= (RelativeLayout) findViewById(R.id.rlayout);
+        dirName= (TextView) findViewById(R.id.dir_name);
+        dirCount=(TextView)findViewById(R.id.dir_count);
     }
 
 }
